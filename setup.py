428ac72e4552edf67e7fed38ba296abddd4b7123
Nastya <nastya@213.local>
2020-02-01 14:36:31 +0100

    чисто по фану

--- setup.py
+++ setup.py
@@ -20,10 +20,6 @@ Unsupported Python version
 This version of Django REST Framework requires Python {}.{}, but you're trying
 to install it on Python {}.{}.
 
-This may be because you are using a version of pip that doesn't
-understand the python_requires classifier. Make sure you
-have pip >= 9.0 and setuptools >= 24.2, then try again:
-
     $ python -m pip install --upgrade pip setuptools
     $ python -m pip install djangorestframework
 
